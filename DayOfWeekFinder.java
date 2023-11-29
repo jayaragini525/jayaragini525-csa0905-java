3e506d99dea5f89f931c7922877dff15c44f7b4f
jayaragini525 <113323348+jayaragini525@users.noreply.github.com>
2023-11-29 19:42:58 +0530

    Add files via upload

---
+++ DayOfWeekFinder.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.time.LocalDate;
+import java.time.format.TextStyle;
+import java.util.Locale;
+class DayOfWeekFinder {
+    public static void main(String[] args) {
+        Scanner s= new Scanner(System.in);
+
+        System.out.print("Enter the day: ");
+        int day = s.nextInt();
+
+        System.out.print("Enter the month: ");
+        int month = s.nextInt();
+
+        System.out.print("Enter the year: ");
+        int year = s.nextInt();
+        s.close();
+        LocalDate date = LocalDate.of(year, month, day);
+        String dayOfWeek = date.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.ENGLISH);
+        System.out.println("The day of the week is: " + dayOfWeek);
+    }
+}
